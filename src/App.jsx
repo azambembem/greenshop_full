a95498c0faa2d5790491c68a3f49f7c0559c02ca
azambembem <azamjan980713@gmail.com>
2024-06-16 13:33:14 +0900

    feat: navbar, sidenav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+import SliderSection from "./components/Navbar/slider-section";
+import SideNav from "./components/sidenav";
+
 const App = () => {
-  return <div> Add</div>;
+  return (
+    <div>
+      <Navbar />
+      <SliderSection />
+      <SideNav />
+    </div>
+  );
 };
 
 export default App;
